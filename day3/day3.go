51bfbc69f9c2d7b8be9703332273f636fccc77be
Leo <lpguercio@gmail.com>
2020-12-03 22:36:56 +0100

    Solve day 3 puzzle

---
+++ day3/day3.go
@@ -0,0 +1,66 @@
+package day3
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const day3Problem = "./day3/day3_problem"
+
+type treeCounter struct {
+	treeLayout []string
+}
+
+func NewTreeCounter() (*treeCounter, error) {
+	f, err := os.Open(day3Problem)
+	if err != nil {
+		return nil, fmt.Errorf("Error opening the file: %w", err)
+	}
+	defer f.Close()
+
+	lines := []string{}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return &treeCounter{lines}, nil
+}
+
+func (t *treeCounter) CountTrees(right, down int) int {
+	trees := 0
+	rightStep := right
+	rowLen := 0
+
+	for i := 0; i < len(t.treeLayout); i += down {
+		row := t.treeLayout[i]
+		if rowLen == 0 {
+			rowLen = len(row)
+			continue
+		}
+		if row[rightStep] == '#' {
+			trees += 1
+		}
+		rightStep += right
+		if rightStep >= rowLen {
+			rightStep -= rowLen
+		}
+	}
+
+	return trees
+}
+
+func (t *treeCounter) MultiplyCount() int {
+	rules := []struct{ right, down int }{
+		{1, 1},
+		{3, 1},
+		{5, 1},
+		{7, 1},
+		{1, 2},
+	}
+	multiplied := 1
+	for _, pair := range rules {
+		multiplied = multiplied * t.CountTrees(pair.right, pair.down)
+	}
+	return multiplied
+}
--- main.go
+++ main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/mountolive/advent2020/day1"
 	"github.com/mountolive/advent2020/day2"
+	"github.com/mountolive/advent2020/day3"
 )
 
 func main() {
-	// DAY 1
+	fmt.Println("-----------DAY 1-------------")
 	fmt.Println(day1.SolutionPartOne())
 	fmt.Println(day1.SolutionPartTwo())
-	// DAY 2
+	fmt.Println("-----------DAY 2-------------")
 	fmt.Println(day2.ValidPasswordsPart1())
 	fmt.Println(day2.ValidPasswordsPart2())
+	fmt.Println("-----------DAY 3-------------")
+	treeCounter, err := day3.NewTreeCounter()
+	if err != nil {
+		log.Fatalf("Error creating the tree counter: %w", err)
+	}
+	fmt.Println(treeCounter.CountTrees(3, 1))
+	fmt.Println(treeCounter.MultiplyCount())
 }
